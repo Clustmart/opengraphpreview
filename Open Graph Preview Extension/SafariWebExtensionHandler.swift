5c586c0f3592388b18111a50b95bee0b2e500584
Digital Travel Guide <contact@romaniatravel.guide>
2023-01-22 16:48:46 +0200

    Initial Commit

---
+++ Open Graph Preview Extension/SafariWebExtensionHandler.swift
@@ -0,0 +1,24 @@
+//
+//  SafariWebExtensionHandler.swift
+//  Open Graph Preview Extension
+//
+//  Created by Paul Wasicsek on 22.01.2023.
+//
+
+import SafariServices
+import os.log
+
+class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
+
+    func beginRequest(with context: NSExtensionContext) {
+        let item = context.inputItems[0] as! NSExtensionItem
+        let message = item.userInfo?[SFExtensionMessageKey]
+        os_log(.default, "Received message from browser.runtime.sendNativeMessage: %@", message as! CVarArg)
+
+        let response = NSExtensionItem()
+        response.userInfo = [ SFExtensionMessageKey: [ "Response to": message ] ]
+
+        context.completeRequest(returningItems: [response], completionHandler: nil)
+    }
+
+}
---
+++ Open Graph Preview/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  Open Graph Preview
+//
+//  Created by Paul Wasicsek on 22.01.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
